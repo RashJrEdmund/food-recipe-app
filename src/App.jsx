be217fb037b71b55f51b81f4c69e4df5d9b2c222
rash <orashusedmund@gmail.com>
2023-02-13 19:34:58 +0100

    done initial mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
+/* eslint-disable react/jsx-pascal-case */
 import './App.css';
+import React from 'react';
+import MyFoodContext from './context/MyContext';
 import FoodContainer from './components/FoodContainer';
 
 function App() {
+  const [foodData, setFoodData] = React.useState([
+    {
+      name: 'Garri',
+      img: 'src',
+      recipe: ['step 1', 'step 2', 'step 3', '...step n'],
+    },
+    {
+      name: 'Garri',
+      img: 'src',
+      recipe: ['step 1', 'step 2', 'step 3', '...step n'],
+    },
+    {
+      name: 'Garri',
+      img: 'src',
+      recipe: ['step 1', 'step 2', 'step 3', '...step n'],
+    },
+    {
+      name: 'Garri',
+      img: 'src',
+      recipe: ['step 1', 'step 2', 'step 3', '...step n'],
+    },
+    {
+      name: 'Garri',
+      img: 'src',
+      recipe: ['step 1', 'step 2', 'step 3', '...step n'],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <h1 className="App-header">
-        <span className="span1">The</span>
-        <span className="span2">Food Recipe App</span>
-      </h1>
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <MyFoodContext.Provider value={{ foodData, setFoodData }}>
+      <div className="App">
+        <h1 className="App-header">
+          <span className="span1">The</span>
+          <span className="span2">Food Recipe App</span>
+        </h1>
 
-      <p className="below-are">Below are a minor compilation of food recipes</p>
+        <p className="below-are">
+          Below are a minor compilation of food recipes
+        </p>
 
-      <FoodContainer />
-    </div>
+        <FoodContainer />
+      </div>
+    </MyFoodContext.Provider>
   );
 }
 
